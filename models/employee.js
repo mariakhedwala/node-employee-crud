1ba7ff40aee6428a0a8ea6b3a15c905f24ecf22c
Maria Khedwala <mariakhedwala@gmail.com>
2023-06-03 01:51:28 +0530

    emp login works

--- models/employee.js
+++ models/employee.js
@@ -7,7 +7,6 @@ const empSchema = new Schema({
   emp_name: { type: String, required: true },
   emp_email: { type: String, required: true, unique: true },
   emp_password: { type: String, required: true, minlength: 6 },
-  emp_photo: { type: String, required: false },
   emp_designation: { type: String, required: true },
   emp_role: { type: String, required: true },
   emp_address: { type: String, required: true },
